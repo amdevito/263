8aa17f15ec03f119f5b4e0d9e101d028975e947e
amdevito <amdevitoam@gmail.com>
2021-01-22 09:02:18 -0500

    strange uncaught type error

---
+++ Activities/wheresSausageDog/js/Animal.js
@@ -0,0 +1,20 @@
+class Animal {
+  contructor(x, y, animalImage) {
+    this.x = x;
+    this.y = y;
+    this.image = animalImage;
+    this.angle = 0;
+  }
+  update() {
+    this.display();
+  }
+
+  display() {
+    push();
+    imageMode(CENTER);
+    translate(this.x, this.y);
+    rotate(this.angle);
+    image(this.image, 0, 0);
+    pop();
+  }
+}
--- Activities/wheresSausageDog/js/script.js
+++ Activities/wheresSausageDog/js/script.js
@@ -1,20 +1,48 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+263: Activity 1 - Where's Sausage Dog
+Alana DeVito
 
-Here is a description of this template p5 project.
+A bunch of Animal objects with a 'hidding' sausage dog that you can find by clicking.
+Sausage dog spins when found.
 **************************************************/
+"use strict";
+// set constants as number of images for the animals in program and the number displayed overall
+const NUM_ANIMAL_IMAGES = 10;
+const NUM_ANIMALS = 100;
 
-// setup()
+//set arrays for images and the duplicated images of animals
+let animalImages = [];
+let animals = [];
+
+///
+function preload() {
+  for (let i = 0; i < NUM_ANIMAL_IMAGES; i++) {
+    let animalImage = loadImage(`assets/images/animal${i}.png`);
+    animalImages.push(animalImage);
+  }
+}
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  //create the animals
+  for (let i = 0; i < NUM_ANIMALS; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    let animalImage = random(animalImages);
+    let animal = new Animal(x, y, animalImage);
+    animals.push(animal);
+  }
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(255, 255, 0);
 
+  for (let i = 0; i < animals.length; i++) {
+    animals[i].update();
+  }
 }
