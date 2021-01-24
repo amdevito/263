79440e6a28d6644db2f657b9a5d66e923974f779
amdevito <amdevitoam@gmail.com>
2021-01-23 21:49:47 -0500

    E1: Created intro screen to click into, or start the game. sonicDeath sound still not working.

--- Exercises/wheres-sausage-dog-game+/js/script.js
+++ Exercises/wheres-sausage-dog-game+/js/script.js
@@ -2,6 +2,7 @@
 263: Exercise 1 - Find the Nintendo World Intruder!!!
 Alana DeVito
 
+Enter screen with instruction to press shift to begin the search game.
 A bunch of classic Nintendo characters with a 'hidding' Sonic the Hedgehog that you can find by clicking.
 Sonic spins and moves down and off screen when found. Sonic Death song plays.
 **************************************************/
@@ -21,6 +22,20 @@ let nintendo = undefined;
 
 let sonicDeath = undefined;
 
+let enterScreen = {
+  string: `Find the Nintendo World Intruder!!! \n Please click <SHIFT> to Begin the SEARCH!`,
+  x: undefined,
+  y: undefined,
+  vx: undefined,
+  vy: undefined,
+  size: undefined,
+};
+
+//set inital state
+let state = `enter`; //different states: enter>title>level1>gameover>win>level2>level3
+
+//Set up Functions
+
 ///
 function preload() {
   for (let i = 0; i < NUM_CHARACTER_IMAGES; i++) {
@@ -30,7 +45,7 @@ function preload() {
   sonicImage = loadImage(`assets/images/sonic.gif`);
   nintendo = loadImage(`assets/images/welcome.gif`);
 
-  soundFormats("wav", "mp3");
+  soundFormats("wav", "mp3", "ogg");
 
   sonicDeath = loadSound(`assets/sounds/sonicDeath.mp3`);
 }
@@ -50,11 +65,48 @@ function setup() {
   let x = random(0, width);
   let y = random(0, height);
   sonic = new Sonic(x, y, sonicImage);
+
+  setUpEnterScreen();
 }
 
 function draw() {
   background(nintendo);
 
+  if (keyIsDown(SHIFT)) {
+    state = `game`;
+  }
+
+  if (state === `enter`) {
+    enterStart();
+  } else if (state === `game`) {
+    gameStart();
+  }
+}
+
+function setUpEnterScreen() {
+  enterScreen.x = width / 2;
+  enterScreen.y = 200;
+  enterScreen.vx = 5;
+  enterScreen.vy = 1;
+  enterScreen.size = 30;
+}
+
+function enterStart() {
+  // set up click to enter screen
+
+  background(nintendo);
+  textSize(enterScreen.size);
+  fill(255);
+  textAlign(CENTER, CENTER);
+  textStyle(BOLD);
+  textFont("Monaco");
+  stroke(0, 0, random(0, 255));
+  strokeWeight(10);
+
+  text(enterScreen.string, enterScreen.x, enterScreen.y);
+}
+
+function gameStart() {
   for (let i = 0; i < characters.length; i++) {
     characters[i].update();
   }
