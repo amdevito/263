e681b7dbdccbb1d24f64600ae2014be2b31dd5f8
amdevito <amdevitoam@gmail.com>
2021-02-02 19:30:45 -0500

    E2: Editing Hint class design and style elements.

--- Exercises/Slamina-GAME/js/Hint.js
+++ Exercises/Slamina-GAME/js/Hint.js
@@ -1,27 +1,27 @@
-//set class for the Characters and the constructor elements
+//set class for the hints and the constructor elements
 class Hint {
   constructor(x, y, hint) {
     this.x = x;
     this.y = y;
     this.string = hint;
   }
-  //update each new character with a new display element
+  //update each new hint with a new display element
   update() {
     this.display();
   }
 
-  //set the character's image aspects, size, location, position
+  //set the hint's image aspects, size, location, position
   display() {
     push();
     stroke(0, 0, random(0, 255));
     // rect(30, 20, 55, 55, 20, 15, 10, 5); //(x coorinate, y coordinate, width of rect, height of rect, radius of topleft corner, radius of topright corner, radius of bottomright corner, radius of bottom left corner)
-    textSize(30);
+    textSize(15);
     fill(255);
     textAlign(CENTER, CENTER);
     textStyle(BOLD);
-    textFont("GEORGIA");
+    textFont("Tahoma");
     stroke(0, 0, random(0, 255));
-    strokeWeight(10);
+    strokeWeight(2);
     text(this.string, this.x, this.y);
     pop();
   }
