647454115a6f9c24fa4a2c249987d931a218118b
amdevito <amdevitoam@gmail.com>
2021-02-06 20:34:40 -0500

    E2: Idea created. Mock up for a 'Mapping the Montreal MusicScape' mobile app. Json file with Montreal neighbourhoods and audio 'gems' to find per each neighbourhood. description entered/summarized. Created folders for diff audio 'gems'. Loaded newly made JSON file.

--- Exercises/spyProfileGeneratorPLUS/js/script.js
+++ Exercises/spyProfileGeneratorPLUS/js/script.js
@@ -1,7 +1,12 @@
 "use strict";
 
 /*****************
-Spy Profile Generator PLUS
+Spy Profile Generator PLUS -- Mapping the Montreal Musicscape
+-- a audio scavenger hunt
+--- walk through the city listening to an ambient soundscape until you enter certain locations. A cue sound indicates that you are getting close and the closer (will get louder and more frequent as you get closer to the 'audioGem')
+
+(Mock Up using location relative to the user's starting point. Eventually these will be connected to specific neighbourhood locations in Montreal.)
+- although you must go to these locations to initially find the items, once found you can access them again from anywhere (they are saved to your local storage in your profile. you can also reset your storage to start fresh.)
 Alana
 Brief:
 - *use geolocation api to find sound files items in your general vacinity
@@ -11,6 +16,29 @@ Brief:
 - *Allow the user to selectively regenerate specific categories in the profile (by clicking on them? with keyboard keys? with their voice?) to get one they like
     *** could this be what kind of oudn pieces they would like to hear?
       - ambient, playlist, interview, soundscape, experimental?
+
+      Enter your name and password please.
+
+      What kind of Montreal Audio Gem would you like to discover today?
+      Show me my old profile.
+      Or
+      Choose:
+      Random
+      Soundscape
+      Interview
+      Story
+      Playlist
+
+      How would you like to find it?
+      Mystery walk (listen to a ambient playlist with abstract audio direction only. A audio signal will get louder or quieter as you get closer to an item.)
+      Direct me with words.
+      Show me a map. ( please be careful).
+
+      **show current profile with the items choices***
+
+      Finding location...
+
+
 
 
 Darius Kazemi's corpora project:
@@ -26,12 +54,15 @@ https://github.com/dariusk/corpora/
 
 // The spy profile data while the program is running
 let spyProfile = {
+  //change to montrealMuMapProfile
   name: `**REDACTED**`,
-  alias: `**REDACTED**`,
-  secretWeapon: `**REDACTED**`,
-  password: `**REDACTED**`,
+  alias: `**REDACTED**`, //my neighbourhood
+  secretWeapon: `**REDACTED**`, //number of audio gems Collected
+  password: `**REDACTED**`, //user chooses own password (just double the user prompts)
+  //current gem hunt: --if choose random, generate randomly, else, choose in app, not in prompt
 };
 // Variables to store JSON data for generating the profile
+//change to variables to store JSON data per neighbourhood
 let instrumentData = undefined;
 let objectData = undefined;
 let tarotData = undefined;
@@ -40,9 +71,7 @@ let tarotData = undefined;
 Loads the JSON data used to generate the profile
 */
 function preload() {
-  tarotData = loadJSON(
-    `https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json`
-  );
+  locationData = loadJSON(`assets/data/location_data.json`);
   objectData = loadJSON(
     `https://raw.githubusercontent.com/dariusk/corpora/master/data/objects/objects.json`
   );
