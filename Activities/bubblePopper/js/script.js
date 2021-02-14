ae2875b2b0f1d1726926e101373250d3f2467c61
amdevito <amdevitoam@gmail.com>
2021-02-13 22:42:57 -0500

    A4: Started Bubble Popper Activity

---
+++ Activities/bubblePopper/js/script.js
@@ -0,0 +1,15 @@
+/**************************************************
+A4: Bubble Popper
+Alana DeVito
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {}
