8675347981fb11b747c9374732ef26d777a0ddac
amdevito <amdevitoam@gmail.com>
2021-02-16 18:09:31 -0500

    E4: Finished bubblePopperPLUS

--- Exercises/bubblePopperPLUS/js/script.js
+++ Exercises/bubblePopperPLUS/js/script.js
@@ -3,16 +3,18 @@ E4: Bubble Popper PLUS
 Alana DeVito
 
 Pop bubbles with your index finger as a pin or change to a hand tool and
-bounce them around.
+bounce them around. Change tools by pinching index finger and thumb together for a quick second and then open hand again.
 
 Brief:
-- change colour when bubbles popped.
+- change colour when bubbles popped (gets more purple and then more green)
 
-- popping sound when bubble popped. Colour of bubble changes when popped?
+- popping sound when bubble popped.
+
+- boing sound when bubble bounces
 
 - start screen
 
-- User closes hand, pin turns into hand and you can bounce bubbles downwards instead.
+- User pinches index and thumb together for a short moment, pin turns into hand and you can bounce bubbles downwards instead.
 Bounced bubble flashes greenish.
 
 
@@ -21,6 +23,7 @@ Bounced bubble flashes greenish.
 //store user's webcam
 let video = undefined;
 
+//store handpose tool
 let handpose = undefined;
 
 //the bubble
@@ -28,6 +31,9 @@ let bubble = undefined;
 
 //bubble sound
 let popSound = undefined;
+
+//boing sound
+let boingSound = undefined;
 
 //hand image tool
 let handTool = undefined;
@@ -59,7 +65,7 @@ let enterScreen = {
 };
 
 // How close the thumb tip should be to the index tip to change tools
-const MIN_CHANGE_DISTANCE = 100;
+const MIN_CHANGE_DISTANCE = 80;
 
 //whether or not the user can change the tool currently
 let canChange = true;
@@ -68,18 +74,20 @@ let canChange = true;
 function preload() {
   popSound = loadSound(`assets/sounds/pop.ogg`);
   handTool = loadImage(`assets/images/handTool.png`);
+  boingSound = loadSound(`assets/sounds/boing.wav`);
 }
 
 function setup() {
   createCanvas(640, 480);
 
+  //set values to the instruction screen by calling this function
   setUpEnterScreen();
 
-  //access user;s webcam
+  //access user's webcam
   video = createCapture(VIDEO);
   video.hide();
 
-  //load the andpose model
+  //load the handpose model
 
   handpose = ml5.handpose(video, { flipHorizontal: true }, function () {
     console.log("Model Loaded.");
@@ -91,7 +99,7 @@ function setup() {
     predictions = results;
   });
 
-  // our bubble
+  // the bubble
   bubble = {
     x: random(width),
     y: height,
@@ -107,6 +115,7 @@ function setup() {
 function draw() {
   background(0);
 
+  //set states to change between start screen and game with different tools
   if (state === `enter`) {
     enterStart();
   } else if (state === `gamePin`) {
@@ -118,10 +127,11 @@ function draw() {
   }
 }
 
+//general gameStart function with the handpose mapping
 function gameStart() {
   if (predictions.length > 0) {
     let hand = predictions[0];
-
+    //set the values to the digits of the hand as variables
     let index = hand.annotations.indexFinger;
 
     let tip = index[3];
@@ -156,16 +166,19 @@ function gameStart() {
     } else if (distIndexThumb > MIN_CHANGE_DISTANCE && !canChange) {
       canChange = true;
     }
+
+    //draw the bubble
     push();
     fill(bubble.r, bubble.g, bubble.b);
     noStroke();
     ellipse(bubble.x, bubble.y, bubble.size);
     pop();
 
-    console.log(distIndexThumb);
+    // console.log(distIndexThumb);// use this to calibrate the MIN_CHANGE_DISTANCE
   }
 }
 
+//drawPin game function
 function drawPin() {
   push();
   noFill();
@@ -189,8 +202,10 @@ function drawPin() {
     bubble.y = height;
     if (bubble.r < 255) {
       bubble.r += 30;
+      //once the red value reaches 254, start increasing the green value instead
     } else if (bubble.r > 254 && bubble.g < 255) {
       bubble.g += 30;
+      // reset colors
     } else {
       bubble.r = 0;
       bubble.g = 0;
@@ -202,6 +217,7 @@ function drawPin() {
   bubble.x += bubble.vx;
   bubble.y += bubble.vy;
 
+  //if bubble goes off top of screen reset to bottom of screen
   if (bubble.y < 0) {
     bubble.x = random(width);
     bubble.y = height;
@@ -216,9 +232,11 @@ function drawHand() {
   pop();
 
   //check bubble bounce
-  // change colour when popping
+  // change to flashing green when bouncing
   let d = dist(tipX, tipY, bubble.x, bubble.y);
   if (d < bubble.size / 2) {
+    boingSound.play();
+    ///bounce bubble down and flash green
     bubble.vy = 10;
     bubble.g = random(0, 255);
   }
@@ -227,16 +245,21 @@ function drawHand() {
   bubble.x += bubble.vx;
   bubble.y += bubble.vy;
 
+  //once bubble bounces to the bottom height of screen, send back up
   if (bubble.y > height) {
     bubble.vy = -3;
+    boingSound.play();
   }
 }
 
+//when mouse pressed in the start screen start game play
 function mousePressed() {
   if (state === `enter`) {
     state = `gamePin`;
   }
 }
+
+//set values for enterscreen
 function setUpEnterScreen() {
   enterScreen.x = width / 2;
   enterScreen.y = 200;
@@ -244,7 +267,7 @@ function setUpEnterScreen() {
   enterScreen.vy = 1;
   enterScreen.size = 15;
 }
-
+//set up and draw opening instruction screen
 function enterStart() {
   background(0);
   textSize(enterScreen.size);
