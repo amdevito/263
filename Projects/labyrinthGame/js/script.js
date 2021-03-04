bca1fbcc5469e8ea0c28ffcd4cedb3acf86100ee
amdevito <amdevitoam@gmail.com>
2021-03-04 16:26:13 -0500

    P1: Editing ReadMe file and discription.

--- Projects/labyrinthGame/js/script.js
+++ Projects/labyrinthGame/js/script.js
@@ -6,20 +6,22 @@ By: Alana DeVito
 
 You are the main character from the 1984 movie, Labyrinth.
 
-You find out your brother, Toby has disappeared and need to search the labyrinth for characters and items in order to find the Goblin King, Jareth and get your baby brother, Toby back!
+You find out your baby brother, Toby has disappeared and need to search the labyrinth for characters and items in order to find the Goblin King, Jareth and get your baby brother, Toby back!
 
 - enter your name and password, which is then saved locally to be recalled later.
 - Search the different scenes using the drop down menus for items and characters to help you on your way.
 - you need to find 7 characters and 4 items by the end of the game or else you can't face off against Jareth and are forced to start again.
 
-- Becareful where you look because there are traps that will force you to start over!
-- hit the advance scene button when you are ready to look in a different location.
+- at the top of each scene/level, a picture from that part of the movie is shown and you are given the storyline of that scene. Press the start your search button to advance to the searching aspect of that level.
+
+- Be careful where you look because there are traps that will force you to start over!
+- hit the advance to next scene button when you are ready to look in a different location.
 
 - press a button to see the map of the labyrinth, but you only see an optical illusion.
 
-- annyang voice recognition employed for the closing scene where the user has to face off against Jareth. User beats Jareth by saying the phrase 'you have no power over me' which they would have learned in the first scene search when they picked up the book, Labyrinth and read that line.
+- Annyang voice recognition employed for the closing scene where the user has to face off against Jareth. User beats Jareth by saying the phrase 'you have no power over me' which they would have learned in the first scene search when they picked up the book, Labyrinth and read that line.
 
-- your geolocation is visable, telling you where you are in the labyrinth! - I decided that having someone walk around their space may be impractical right now, but I still wanted to try and utilize a geolocation library so I have kept that in the application for visual UI purposes mainly.
+- your geolocation is visible, telling you where you are in the labyrinth (actually in real-life)! - I decided that having someone walk around their space may be impractical right now, but I still wanted to try and utilize a geolocation library so I have kept that in the application for visual UI purposes mainly.
 
 ******************/
 //set beginning state for the game
