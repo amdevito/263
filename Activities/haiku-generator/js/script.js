ffe687c512304c7a62444b91dd5c49c3c2b92c78
amdevito <amdevitoam@gmail.com>
2021-03-07 11:05:01 -0500

    A6: Haiku generator printing my words to the canvas.

--- Activities/haiku-generator/js/script.js
+++ Activities/haiku-generator/js/script.js
@@ -1,11 +1,42 @@
 /**
-Title of Project
-Author Name
+Haiku generator
+Alana
+Generates a random haiku
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
 */
 
 "use strict";
 
-// Code goes here
+let fiveSyllableLines = [
+  `Gosh, all love is free`,
+  `The plants breathe their best`,
+  `Everything is rare`,
+  `We to be and I`,
+  `All I see is you`,
+];
+
+let sevenSyllableLines = [
+  `I cannot love anyone well`,
+  `There is nothing but nothing`,
+  `Anywhere love is, you are`,
+  `Best be things I cannot see`,
+  `Earth is witch but which is earth`,
+];
+
+let line1 = random(fiveSyllableLines);
+let line2 = random(sevenSyllableLines);
+let line3 = random(fiveSyllableLines);
+
+let line1P = document.getElementById(`line-1`);
+let line2P = document.getElementById(`line-2`);
+let line3P = document.getElementById(`line-3`);
+
+line1P.innerText = line1;
+line2P.innerText = line2;
+line3P.innerText = line3;
+
+function random(array) {
+  let index = Math.floor(Math.random() * array.length);
+  return array[index];
+  console.log(index);
+}
