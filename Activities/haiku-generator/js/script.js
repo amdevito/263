5bc57e0297993e52580d5ceb19a26100d660b8d4
amdevito <amdevitoam@gmail.com>
2021-03-07 11:52:00 -0500

    A6: Haiku lines are fading in and out. Activity and lesson complete.

--- Activities/haiku-generator/js/script.js
+++ Activities/haiku-generator/js/script.js
@@ -51,7 +51,18 @@ function fadeOut(element, opacity) {
       fadeOut(element, opacity);
     });
   } else {
-    ///something to do when it's faded out...
+    setNewLine(element);
+    fadeIn(element, 0);
+  }
+}
+
+function fadeIn(element, opacity) {
+  opacity += 0.01;
+  element.style[`opacity`] = opacity;
+  if (opacity < 1) {
+    requestAnimationFrame(function () {
+      fadeIn(element, opacity);
+    });
   }
 }
 
