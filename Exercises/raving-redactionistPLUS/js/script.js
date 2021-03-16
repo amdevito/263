3a5cb2694c37077960ecdcc0a49c5c444b9904a2
amdevito <amdevitoam@gmail.com>
2021-03-16 16:07:02 -0400

    E6: Completed e6 Raving redactionistPLus. Just need to comment.

--- Exercises/raving-redactionistPLUS/js/script.js
+++ Exercises/raving-redactionistPLUS/js/script.js
@@ -1,10 +1,10 @@
 // Exercise 6: Raving Redactionist++
+//A top secret document is shown on screen. On top of the document an animated text types, "** TOP SECRET! DO NOT EXPOSE!**" at the top of the page. Shortly after this happens the blocked out text is revealed. You must click on that exposed text to block it out again. When you click on the text to block it out you can hear the sound of the marker move across the page.
 //Brief:
-//X change the text, and redacted different text
-//X Improve visual presentaiton by working with CSS and HTML - do the typing effect text types out** TOP SECRET DO NOT EXPOSE **first, then the revealing happens - need interval to wait before revelation is first called
-//
-//Click and drag to reveal rather than clicking
-//audio of marker squeeking across page when user clicks and drags
+//[X] 'Find a different text to be redacted' Change the text, and redacted different text
+//[X] 'Improve visual presentaiton by working with CSS and HTML' -  text types out ''** TOP SECRET! DO NOT EXPOSE! **' first, then the revealing happens - revelation is called after the text type animation is complete.
+
+//[X] "Add audio to the experience" - audio of marker squeaking across page when user clicks on the revealed text.
 
 "use strict";
 
