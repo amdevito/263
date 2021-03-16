40661c961fd46bce8d12bd61aa37740e788dc4c9
amdevito <amdevitoam@gmail.com>
2021-03-16 14:11:22 -0400

    E6: Changed Lorem Ipsum text to english translation suggested by Google translate.

--- Exercises/raving-redactionistPLUS/js/script.js
+++ Exercises/raving-redactionistPLUS/js/script.js
@@ -1,11 +1,25 @@
+// Exercise 6: Raving Redactionist++
+//Brief:
+//text types out first, then the revealing happens - need timer to wait before revelation is first called
+// Improve visual presentaiton by working with CSS and HTML - do the typing effect
+//Click and drag to reveal rather than clicking
+//audio of marker squeeking across page when user clicks and drags
+
 "use strict";
 
+let markerCrossOut = undefined;
+
+function preload() {
+  markerCrossOut = loadSound(`assets/sounds/marker.wav`);
+}
+
 $(`.top-secret`).on(`click`, redact);
-setInterval(revelation, 500);
+setInterval(revelation, 1000);
 
 function redact(event) {
   $(this).removeClass(`revealed`);
   $(this).addClass(`redacted`);
+  markerCrossOut.play(); //
 }
 
 function revelation() {
