55d7d28937ea93f861744fd1fc0292395becd4c3
amdevito <amdevitoam@gmail.com>
2021-03-20 09:30:02 -0400

    E6: Created brief for the JQUERY elements being applied to P2: prototype -

--- Exercises/generative-weatherScape_codeTakerPlus/js/weatherScape.js
+++ Exercises/generative-weatherScape_codeTakerPlus/js/weatherScape.js
@@ -1,25 +1,9 @@
-// Generative soundscape based on the weather in your location  - choose same as or opposite to (i.e. sometimes you want sunny music on a rainy day!)
+// Generative soundscape prototype using JQUERY elements
 //
-// Reference sources:
-//
-// Generative music box project at Udemy
-//
-// And
-//
-// https://www.devbridge.com/articles/tonejs-coding-music-production-guide/
-//
-//
-// https://medium.com/dev-red/tutorial-lets-make-music-with-javascript-and-tone-js-f6ac39d95b8c
-//
-// Ideas to do:
-// - Replace the packages of mp3 sounds with the tone.js synthesizer
-// - Visualization is ripples like in water, when the ripples overlap, the colours blend to make new colours
-// - Need to get a google weather API
-// - Then decide which elements from that API control which musical part. - frequency of notes, key, octave, distribution of note length, number of types of sounds used (1-3), bpm (always fairly slow but fluctuating between 60-90?.
-// - Background colour should very slowly change/cycle
-// - might be nice to make this 3 d ? the circles change to spheres?
-///- when person launches the app - a tone is generated depending on their long and lat location in the world - that will be the base key? then the next note is determined by the temp? or the mode? something deterimes the progression at every 4 bars?
-///LOOK INTO THIS-maybe have the circles shrink at a setInterval? so that it has a sort of breathing pulsing effect
+// Brief:
+// 1. add modal instructions dialogue when the page loads. Explains to the user what the app is, what it does and how it interacts. (see prisoner activity elements.)
+//2. drag and drop circles to change keys and modes of the soundscape
+//3. background color slowly modulates when you open a hidden modal with radio clickable instructions and choose modulate background colours via current wind speed? oscillates.
 
 window.onload = function () {
   //what gets passed to the loadBuffer method is the URL of the file containing the sound and the index of that sound in the list.
