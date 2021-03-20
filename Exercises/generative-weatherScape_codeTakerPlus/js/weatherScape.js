0aabb8c35927aebc2b5fd0d049777ce70604d334
amdevito <amdevitoam@gmail.com>
2021-03-20 10:18:20 -0400

    E6: Created dialog box as a modal for the intro to the application.

--- Exercises/generative-weatherScape_codeTakerPlus/js/weatherScape.js
+++ Exercises/generative-weatherScape_codeTakerPlus/js/weatherScape.js
@@ -4,6 +4,9 @@
 // 1. add modal instructions dialogue when the page loads. Explains to the user what the app is, what it does and how it interacts. (see prisoner activity elements.)
 //2. drag and drop circles to change keys and modes of the soundscape
 //3. background color slowly modulates when you open a hidden modal with radio clickable instructions and choose modulate background colours via current wind speed? oscillates color values slllllooowlly.
+$(`#introduction-dialog`).dialog({
+  modal: true,
+});
 
 window.onload = function () {
   //what gets passed to the loadBuffer method is the URL of the file containing the sound and the index of that sound in the list.
