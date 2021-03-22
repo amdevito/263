f9aae8f6cd25dee01044d36ced2865a47ed7d438
amdevito <amdevitoam@gmail.com>
2021-03-22 10:21:07 -0400

    E6: All UI elements using Jquery are on page, but do not effect the scape yet.

--- Exercises/generative-weatherScape_codeTakerPlus/js/weatherScape.js
+++ Exercises/generative-weatherScape_codeTakerPlus/js/weatherScape.js
@@ -1,9 +1,10 @@
 // Generative soundscape prototype using JQUERY elements
 //
 // Brief:
-// 1. X add modal instructions dialogue when the page loads. Explains to the user what the app is, what it does and how it interacts. (see prisoner activity elements.)
-//2. drag and drop modularing ripples on to the tone ripples  to efffects to that element.
-//3. background color slowly modulates when you open a hidden modal with radio clickable instructions and choose modulate background colours via current wind speed? oscillates color values slllllooowlly.
+// 1. X add modal instructions dialogue when the page loads. Explains to the user what the app is, what it does and how it interacts.
+// --- background color slowly modulates when you open a hidden modal with radio clickable instructions and choose modulate background colours via current wind speed? oscillates color values slllllooowlly.
+//2. X drag and drop modularing ripples on to the tone ripples  to add efffects to that element.
+//3. X Hidden dialog box with options to change the scape's tonality - key or mode.
 $(`#introduction-dialog`).dialog({
   modal: true,
   buttons: {
