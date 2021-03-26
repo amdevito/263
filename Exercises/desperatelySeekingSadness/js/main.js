9a3c164c414b5cae5a84d79b8e2feabfe809a6bb
amdevito <amdevitoam@gmail.com>
2021-03-26 19:00:19 -0400

    E7: Started Exercise 7- Desperately Seeking Sadness PLUSPLUS. Defined the brief to accomplish.

--- Exercises/desperatelySeekingSadness/js/main.js
+++ Exercises/desperatelySeekingSadness/js/main.js
@@ -1,9 +1,15 @@
 /**
-Desperately Seeking Sadness
+E7: Desperately Seeking SadnessPLUSPLUS
 
 Alana DeVito
 
 An emoji in search of satisfying sadness in a world of positivity
+
+Brief:
+
+- Add sound effects  to collection and collision (see redactionist code)
+- Choose any examples in the Phaser 3 examples and implement the ideas in the game
+- Change to a different metaphor with different images but the same core idea of collection in a physics based world.
 */
 
 "use strict";
