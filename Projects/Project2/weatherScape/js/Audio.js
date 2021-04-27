b6e9931ece4604cfed70457ce218db57d238cd35
amdevito <amdevitoam@gmail.com>
2021-04-27 15:23:23 -0400

    P2: Audio.js commented and cleaned

--- Projects/Project2/weatherScape/js/Audio.js
+++ Projects/Project2/weatherScape/js/Audio.js
@@ -1,25 +1,25 @@
-//Setting up and storing audio samples in buffer. This will handle the synth construction in the final project.
+//Setting up and storing audio samples in buffer.
 
 let Audio = {
   gainNode: undefined,
   bufferList: undefined,
-  audioContext: new (window.AudioContext || window.webkitAudioContext)(), //object that knows how to decode binary audio data and make hr browser play a sound
+  audioContext: new (window.AudioContext || window.webkitAudioContext)(), //object that knows how to decode binary audio data and make browser play a sound
   //init method initializes the audio objects
   //function takes the array 'bufferList' which is holding the sounds
   init: function (bufferList) {
     this.bufferList = bufferList; //array holding the sounds to play
-    //audio routing create sound>generate sound
-    this.gainNode = this.audioContext.createGain(); //volume
-    this.gainNode.gain.value = 1;
+    //audio routing to create sound/generate sound
+    this.gainNode = this.audioContext.createGain(); //volume/aplitude
+    this.gainNode.gain.value = 1; //full digital amplitude
     this.gainNode.connect(this.audioContext.destination);
   },
-  //call play method to play the sound (done in the View object)
-  //play method takes an index of one of the sounds in the bufferList and prepares that for playing by creating a buffer sounrce
+  //call play method to play the sound
+  //play method takes an index of one of the sounds in the bufferList and prepares that for playing by creating a buffer source
   play: function (i) {
-    let sound = this.audioContext.createBufferSource();
-    sound.connect(this.gainNode);
-    sound.buffer = this.bufferList[i];
-    sound.start(0);
-    sound.stop(this.audioContext.currentTime + 18);
+    let sound = this.audioContext.createBufferSource(); //connect the source
+    sound.connect(this.gainNode); ///source connected to gain
+    sound.buffer = this.bufferList[i]; //fetching the sounds in the bufferList
+    sound.start(0); ///start at first item
+    sound.stop(this.audioContext.currentTime + 18); ///stop audio
   },
 };
